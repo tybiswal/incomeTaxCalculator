0deff4b1fbd72b2fcc03155dd56f3729f20bf737
tybiswal <kanhatheman@gmail.com>
2022-09-02 12:28:53 +0630

    Cleaned up the outputs, added in comments, improved formatting and included future component suggestion.

--- valid1.py
+++ valid1.py
@@ -1,24 +1,35 @@
 # Myanmar Income Tax Calculation
 print('Myanmar Income Tax Calculation')
 yearly_income = int(input('What is your yearly income in Myanmar? : '))
+
 # 20% staff relief
 staff_relief = 0.2
+
 # total relief after staff deduction
 tot_relief = int(float(yearly_income)*staff_relief)
-print("\nTotal Relief is: " + str(tot_relief))
-# will include parental, spousal & child relief in next iteration with boolean inputs
+tot_rf = "{:,}".format(tot_relief)
+print("\nTotal Relief is: \nKyats " + str(tot_rf))
+
+''' 
+here I will include parental, spousal & child relief in next iteration with boolean inputs later
+'''
+
 # This gives us the assessable income
 inc_relief = yearly_income - tot_relief
-print("\nTotal Assessable Income is: " + str(inc_relief))
+inc_rf = "{:,}".format(inc_relief)
+print("Total Assessable Income is: \nKyats " + str(inc_rf))
+
 # these are the tax brackets as per law
 tax_b1 = 2000000
 tax_b2 = 5000000
 tax_b3 = 10000000
 tax_b4 = 20000000
 tax_b5 = 30000000
+
 # clears the variable whenever the code is rerun
 tax_rate = 0.00
 a_tax = 0
+
 # tax rate identifier if-else statement
 if inc_relief <= tax_b1:
     tax_rate = 0.00
@@ -26,29 +37,29 @@ if inc_relief <= tax_b1:
 elif tax_b1 < inc_relief <= tax_b2:
     tax_rate = 0.05
     a_tax = (inc_relief - tax_b1) * tax_rate
-    print("Your Monthly Income Tax is: ")
-    print(a_tax / 12)
 elif tax_b2 < inc_relief <= tax_b3:
     tax_rate = 0.10
     a_tax = ((tax_b2-tax_b1) * 0.05) + ((inc_relief - tax_b2) * tax_rate)
-    print("Your Monthly Income Tax is: ")
-    print(a_tax / 12)
 elif tax_b3 < inc_relief <= tax_b4:
     tax_rate = 0.15
     a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1)+((inc_relief - tax_b3) * tax_rate)
-    print("Your Monthly Income Tax is: ")
-    print(a_tax / 12)
 elif tax_b4 < inc_relief <= tax_b5:
     tax_rate = 0.20
     a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((tax_b4 - tax_b3) * 0.15) + ((inc_relief - tax_b4)
                                                                                                    * tax_rate)
-    print("\nYour Monthly Income Tax is: ")
-    print(a_tax / 12)
 else:
     tax_rate = 0.25
     a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((tax_b4 - tax_b3) * 0.15) + ((tax_b5 - tax_b4) *
                                                                                                    0.2) + ((inc_relief -
                                                                                                             tax_b5) *
                                                                                                            tax_rate)
-    print("\nYour Monthly Income Tax is: ")
-    print(a_tax / 12)
+
+# convert the tax value to integer, to remove the float since currency doesn't support cents and py should round up
+a_tax = int(a_tax)
+a_taxf = "{:,}".format(a_tax) # formats to include number commas
+m_tax = int(a_tax / 12) # monthly value, if we divide by 12
+m_taxf = "{:,}".format(m_tax)
+
+# Prints the annual and monthly income tax
+print("\nYour Annual Income Tax is: \nKyats " + a_taxf)
+print("Your Monthly Income Tax is: \nKyats " + m_taxf)
