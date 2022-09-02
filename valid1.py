a2e6bbcda1e66353a1d1297e6486669e87c080d4
tybiswal <kanhatheman@gmail.com>
2022-09-02 11:34:42 +0630

    Cleanup of the rendered text

--- valid1.py
+++ valid1.py
@@ -5,10 +5,11 @@ yearly_income = int(input('What is your yearly income in Myanmar? : '))
 staff_relief = 0.2
 # total relief after staff deduction
 tot_relief = int(float(yearly_income)*staff_relief)
-print(tot_relief)
+print("\nTotal Relief is: " + str(tot_relief))
 # will include parental, spousal & child relief in next iteration with boolean inputs
 # This gives us the assessable income
 inc_relief = yearly_income - tot_relief
+print("\nTotal Assessable Income is: " + str(inc_relief))
 # these are the tax brackets as per law
 tax_b1 = 2000000
 tax_b2 = 5000000
@@ -21,21 +22,26 @@ a_tax = 0
 # tax rate identifier if-else statement
 if inc_relief <= tax_b1:
     tax_rate = 0.00
+    print("You owe no income tax!")
 elif tax_b1 < inc_relief <= tax_b2:
     tax_rate = 0.05
     a_tax = (inc_relief - tax_b1) * tax_rate
+    print("Your Monthly Income Tax is: ")
     print(a_tax / 12)
 elif tax_b2 < inc_relief <= tax_b3:
     tax_rate = 0.10
     a_tax = ((tax_b2-tax_b1) * 0.05) + ((inc_relief - tax_b2) * tax_rate)
+    print("Your Monthly Income Tax is: ")
     print(a_tax / 12)
 elif tax_b3 < inc_relief <= tax_b4:
     tax_rate = 0.15
     a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1)+((inc_relief - tax_b3) * tax_rate)
+    print("Your Monthly Income Tax is: ")
     print(a_tax / 12)
 elif tax_b4 < inc_relief <= tax_b5:
     tax_rate = 0.20
     a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((tax_b4 - tax_b3) * 0.15) + ((inc_relief - tax_b4) * tax_rate)
+    print("\nYour Monthly Income Tax is: ")
     print(a_tax / 12)
 else:
     tax_rate = 0.25
