182a0580ec998f3e4ab95653848b8581859fd078
tybiswal <kanhatheman@gmail.com>
2022-09-02 11:16:26 +0630

    Working code with more tax brackets included.

--- valid1.py
+++ valid1.py
@@ -2,29 +2,23 @@
 
 print('Myanmar Income Tax Calculation')
 yearly_income = int(input('What is your yearly income in Myanmar? : '))
-
 # 20% staff relief
 staff_relief = 0.2
-
 # total relief after staff deduction
 tot_relief = int(float(yearly_income)*staff_relief)
 print(tot_relief)
 # will include parental, spousal & child relief in next iteration with boolean inputs\
-
 # This gives us the assessable income
 inc_relief = yearly_income - tot_relief
-
 # these are the tax brackets as per law
 tax_b1 = 2000000
 tax_b2 = 5000000
 tax_b3 = 10000000
 tax_b4 = 20000000
 tax_b5 = 30000000
-
 # clears the variable whenever the code is rerun
 tax_rate = 0.00
 a_tax = 0
-
 # tax rate identifier if-else statement
 if inc_relief <= tax_b1:
     tax_rate = 0.00
