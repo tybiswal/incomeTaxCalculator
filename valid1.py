22a55b9fc81a92474ebef4e345a589cdd7aee1b8
tybiswal <kanhatheman@gmail.com>
2022-09-07 10:52:58 +0630

    Converted code into function, and tax brackets into array.

--- valid1.py
+++ valid1.py
@@ -3,9 +3,6 @@ import math
 
 
 def tax_calc():
-    print('\nMyanmar Income Tax Calculation')
-    yearly_income = int(input('What is your yearly income in Myanmar? (Kyats): '))
-
     # Staff relief is 20%->
     staff_relief = int(float(yearly_income) * 0.2)
     print("\nStaff Relief is: \nKyats " + str("{:,}".format(staff_relief)))
@@ -38,41 +35,37 @@ def tax_calc():
     print("Total Assessable Income is: \nKyats " + str("{:,}".format(inc_relief)))
 
     # these are the tax brackets as per law
-    tax_b1 = 2000000
-    tax_b2 = 5000000
-    tax_b3 = 10000000
-    tax_b4 = 20000000
-    tax_b5 = 30000000
+    tax_b = [2000000, 5000000, 10000000, 20000000, 30000000]
 
     # clears the variable whenever the code is rerun
     tax_rate = 0.00
     a_tax = 0
 
     # tax rate identifier if-else statement
-    if inc_relief <= tax_b1:
+    if inc_relief <= tax_b[0]:
         tax_rate = 0.00
         print("You owe no income tax!")
-    elif tax_b1 < inc_relief <= tax_b2:
+    elif tax_b[0] < inc_relief <= tax_b[1]:
         tax_rate = 0.05
-        a_tax = (inc_relief - tax_b1) * tax_rate
-    elif tax_b2 < inc_relief <= tax_b3:
+        a_tax = (inc_relief - tax_b[0]) * tax_rate
+    elif tax_b[1] < inc_relief <= tax_b[2]:
         tax_rate = 0.10
-        a_tax = ((tax_b2 - tax_b1) * 0.05) + ((inc_relief - tax_b2) * tax_rate)
-    elif tax_b3 < inc_relief <= tax_b4:
+        a_tax = ((tax_b[1] - tax_b[0]) * 0.05) + ((inc_relief - tax_b[1]) * tax_rate)
+    elif tax_b[2] < inc_relief <= tax_b[3]:
         tax_rate = 0.15
-        a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((inc_relief - tax_b3) * tax_rate)
-    elif tax_b4 < inc_relief <= tax_b5:
+        a_tax = ((tax_b[1] - tax_b[0]) * 0.05) + ((tax_b[2] - tax_b[1]) * 0.1) + ((inc_relief - tax_b[2]) * tax_rate)
+    elif tax_b[3] < inc_relief <= tax_b[4]:
         tax_rate = 0.20
-        a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((tax_b4 - tax_b3) * 0.15) + (
-                    (inc_relief - tax_b4)
-                    * tax_rate)
+        a_tax = ((tax_b[1] - tax_b[0]) * 0.05) + ((tax_b[2] - tax_b[1]) * 0.1) + ((tax_b[3] - tax_b[2]) * 0.15) + (
+                (inc_relief - tax_b[3])
+                * tax_rate)
     else:
         tax_rate = 0.25
-        a_tax = ((tax_b2 - tax_b1) * 0.05) + ((tax_b3 - tax_b2) * 0.1) + ((tax_b4 - tax_b3) * 0.15) + (
-                    (tax_b5 - tax_b4) *
-                    0.2) + ((inc_relief -
-                             tax_b5) *
-                            tax_rate)
+        a_tax = ((tax_b[1] - tax_b[0]) * 0.05) + ((tax_b[2] - tax_b[1]) * 0.1) + ((tax_b[3] - tax_b[2]) * 0.15) + (
+                (tax_b[4] - tax_b[3]) *
+                0.2) + ((inc_relief -
+                         tax_b[4]) *
+                        tax_rate)
 
     # convert the tax value to integer, to remove the float and round up since currency doesn't support cents
     a_tax = int(math.ceil(a_tax))
@@ -85,5 +78,12 @@ def tax_calc():
     print("Your Monthly Income Tax is: \nKyats " + m_tax_f)
 
 
-tax_calc()
+print('\nMyanmar Income Tax Calculation')
+yearly_income = int(input('What is your yearly income in Myanmar? (Kyats): '))
+if yearly_income == 0:
+    print('You owe no income tax!')
+else:
+    tax_calc()
+
+
 X = input("\n--------------------\nPress Enter to Close")
